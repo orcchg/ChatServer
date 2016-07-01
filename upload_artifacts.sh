8ff28938c154464e8b246e6b717d955d90bea87a
Maxim Alov <alovmax@yandex.ru>
2016-07-01 09:02:49 +0300

    Added upload artifacts script

---
+++ upload_artifacts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp build/api/libapi.so build/client/client build/client/s_client build/crypting/libcryptor.so build/database/libdatabase.co build/parser/libmy_parser.so build/server/server build/sqlite/libsqlite.so maxa@194.190.63.108:/home/maxa/Develop/Server/ChatServer
+
