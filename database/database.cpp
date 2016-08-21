83250f490ec933958be57de18bf7fbb7a9ce7197
Maxim Alov <alovmax@yandex.ru>
2016-08-21 09:02:52 +0300

    License UPDATE

--- database/database.cpp
+++ database/database.cpp
@@ -16,6 +16,14 @@
  *   You should have received a copy of the GNU General Public License
  *   along with this program; if not, write to the Free Software Foundation,
  *   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ *   This program and text files composing it, and/or compiled binary files
+ *   (object files, shared objects, binary executables) obtained from text
+ *   files of this program using compiler, as well as other files (text, images, etc.)
+ *   composing this program as a software project, or any part of it,
+ *   cannot be used by 3rd-parties in any commercial way (selling for money or for free,
+ *   advertising, commercial distribution, promotion, marketing, publishing in media, etc.).
+ *   Only the original author - Maxim Alov - has right to do any of the above actions.
  */
 
 #include <cassert>
