f5c7eeb5ffa7120f0769a885d38a0e0187905af2
Maxim Alov <orcchg@yandex.ru>
2016-09-27 10:41:08 +0300

    Added upload debug shell

---
+++ upload_artifacts_debug.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp build_debug/api/libapi.so build_debug/common/libcommon.so  build_debug/client/client build_debug/client/s_client build_debug/crypting/libcryptor.so build_debug/database/libdatabase.so build_debug/parser/libmy_parser.so build_debug/server/server build_debug/sqlite/libsqlite.so maxa@194.190.63.108:/home/maxa/Develop/Server/ChatServer
+
