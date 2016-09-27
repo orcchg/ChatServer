9cbed1f12d4ae8668f0d6b2a6f57377b78df08e6
Maxim Alov <alovmax@yandex.ru>
2016-09-27 08:48:57 +0300

    Added Monkey client stub

---
+++ test/monkey/monkey.cpp
@@ -0,0 +1,65 @@
+/** 
+ *   HTTP Chat server with authentication and multi-channeling.
+ *
+ *   Copyright (C) 2016  Maxim Alov
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software Foundation,
+ *   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ *   This program and text files composing it, and/or compiled binary files
+ *   (object files, shared objects, binary executables) obtained from text
+ *   files of this program using compiler, as well as other files (text, images, etc.)
+ *   composing this program as a software project, or any part of it,
+ *   cannot be used by 3rd-parties in any commercial way (selling for money or for free,
+ *   advertising, commercial distribution, promotion, marketing, publishing in media, etc.).
+ *   Only the original author - Maxim Alov - has right to do any of the above actions.
+ */
+
+#include <cstring>
+#include "monkey.h"
+
+namespace monkey {
+
+Monkey::Monkey(const std::string& config_file)
+  : Client(config_file) {
+}
+
+Monkey::~Monkey() {
+}
+
+void Monkey::goToMainMenu() {
+}
+
+}  // namespace monkey
+
+/* Main */
+// ----------------------------------------------------------------------------
+int main(int argc, char** argv) {
+  // read configuration
+  std::string config_file = "../client/local.cfg";
+  if (argc >= 2) {
+    char buffer[256];
+    memset(buffer, 0, 256);
+    strncpy(buffer, argv[1], strlen(argv[1]));
+    config_file = std::string(buffer);
+  }
+  DBG("Configuration from file: %s", config_file.c_str());
+
+  // start monkey
+  monkey::Monkey monkey(config_file);
+  monkey.init();
+  monkey.run();
+  return 0;
+}
+
